08c1498a483d6fd6bd93d14b0a0c69021e1c1665
yassinedhib <yassinedhib@esprit.tn>
2023-05-02 19:23:53 +0100

    fixed posts

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@
         use PHPMailer\PHPMailer\PHPMailer;
         use PHPMailer\PHPMailer\Exception; 
         
-        require 'PHPMailer/src/Exception.php';
+        require 'phpmailer/src/Exception.php';
         require 'PHPMailer/src/PHPMailer.php';  
         require 'PHPMailer/src/SMTP.php';
         
